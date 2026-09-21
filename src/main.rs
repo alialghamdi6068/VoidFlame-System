4ceda423790309e8bf7256d44f6585a8770c0c32
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-21 13:14:29 +0300

    Wire async database into Rust runtime

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
 mod config;
+mod database;
 mod dashboard;
 mod discord;
 
 use anyhow::Result;
 use config::Config;
+use database::Database;
 use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            std::env::var("RUST_LOG")
-                .unwrap_or_else(|_| "voidflame_system=info,tower_http=info".into()),
-        )
-        .init();
-
+    tracing_subscriber::fmt().with_env_filter(std::env::var("RUST_LOG").unwrap_or_else(|_| "voidflame_system=info,tower_http=info".into())).init();
     let config = Config::from_env()?;
-    info!("Starting VoidFlame System in Rust");
-
-    let discord = discord::build_client(&config).await?;
+    let database = Database::connect().await?;
+    database.init().await?;
+    info!("Database initialized");
+    let discord = discord::build_client(&config, database.clone()).await?;
     let dashboard = dashboard::serve(config.clone(), discord.cache.clone());
-
     tokio::select! {
         result = discord.start() => result?,
         result = dashboard => result?,
-        _ = tokio::signal::ctrl_c() => {
-            info!("Shutdown signal received");
-        }
+        _ = tokio::signal::ctrl_c() => info!("Shutdown signal received"),
     }
-
     Ok(())
 }
